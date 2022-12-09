e6842e12ee7b02e0280b586f597a7904d4e6c420
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2022-12-08 20:32:44 -0500

    Add route test

--- test/RouterTest.php
+++ test/RouterTest.php
@@ -6,59 +6,64 @@ use Jmarreros\HttpMethods;
 use Jmarreros\Router;
 use PHPUnit\Framework\TestCase;
 
-class RouterTest extends  TestCase{
-	public function test_resolve_basic_route_with_callback_action(){
-		$uri = '/test';
+class RouterTest extends TestCase {
+	public function test_resolve_basic_route_with_callback_action() {
+		$uri    = '/test';
 		$action = fn() => "test";
 
 		$router = new Router();
-		$router->get($uri, $action);
+		$router->get( $uri, $action );
 
-		$this->assertEquals($action, $router->resolve($uri, HttpMethods::GET->value));
+		$route = $router->resolve( $uri, HttpMethods::GET->value );
+		$this->assertEquals( $action, $route->action() );
+		$this->assertEquals( $uri, $route->uri() );
 	}
 
-	public function test_resolve_multiple_basic_routes_with_callback_action(){
+	public function test_resolve_multiple_basic_routes_with_callback_action() {
 		$routes = [
-			'/test' => fn() => 'test',
-			'/foo' => fn() => 'foo',
-			'/bar' => fn() => 'bar',
+			'/test'              => fn() => 'test',
+			'/foo'               => fn() => 'foo',
+			'/bar'               => fn() => 'bar',
 			'/long/nested/route' => fn() => 'long nested',
 		];
 
 		$router = new Router();
 
-		foreach ($routes as $uri => $action){
-			$router->get($uri, $action);
+		foreach ( $routes as $uri => $action ) {
+			$router->get( $uri, $action );
 		}
 
 		foreach ( $routes as $uri => $action ) {
-			$this->assertEquals($action, $router->resolve($uri, HttpMethods::GET->value));
+			$route = $router->resolve( $uri, HttpMethods::GET->value );
+			$this->assertEquals( $action, $route->action() );
 		}
 	}
 
-	public function test_resolve_multiple_basic_routes_with_callback_action_for_different_http_methods(){
+	public function test_resolve_multiple_basic_routes_with_callback_action_for_different_http_methods() {
 		$routes = [
-			[HttpMethods::GET, "/test", fn() => "get"],
-			[HttpMethods::POST, "/test", fn() => "post"],
-			[HttpMethods::PUT, "/test", fn() => "put"],
-			[HttpMethods::PATCH, "/test", fn() => "patch"],
-			[HttpMethods::DELETE, "/test", fn() => "delete"],
+			[ HttpMethods::GET, "/test", fn() => "get" ],
+			[ HttpMethods::POST, "/test", fn() => "post" ],
+			[ HttpMethods::PUT, "/test", fn() => "put" ],
+			[ HttpMethods::PATCH, "/test", fn() => "patch" ],
+			[ HttpMethods::DELETE, "/test", fn() => "delete" ],
 
-			[HttpMethods::GET, "/random/get", fn() => "get"],
-			[HttpMethods::POST, "/random/nested/put", fn() => "post"],
-			[HttpMethods::PUT, "/put/random/route", fn() => "put"],
-			[HttpMethods::PATCH, "/some/patch/route", fn() => "patch"],
-			[HttpMethods::DELETE, "/d", fn() => "delete"],
+			[ HttpMethods::GET, "/random/get", fn() => "get" ],
+			[ HttpMethods::POST, "/random/nested/put", fn() => "post" ],
+			[ HttpMethods::PUT, "/put/random/route", fn() => "put" ],
+			[ HttpMethods::PATCH, "/some/patch/route", fn() => "patch" ],
+			[ HttpMethods::DELETE, "/d", fn() => "delete" ],
 		];
 
 		$router = new Router();
 
-		foreach ($routes as [$method, $uri, $action]){
-			$router->{strtolower($method->value)}($uri, $action);
+		foreach ( $routes as [$method, $uri, $action] ) {
+			$router->{strtolower( $method->value )}( $uri, $action );
 		}
 
-		foreach ($routes as [$method, $uri, $action]){
-			$this->assertEquals($action, $router->resolve($uri, $method->value));
+		foreach ( $routes as [$method, $uri, $action] ) {
+			$route = $router->resolve( $uri, $method->value );
+			$this->assertEquals( $action, $route->action() );
+			$this->assertEquals( $uri, $route->uri() );
 		}
 
 
