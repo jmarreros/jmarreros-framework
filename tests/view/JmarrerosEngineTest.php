f5ed9dde1b0611d7cb1c06e3e65becb7b1998b08
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2023-01-12 11:06:32 -0500

    Jmarreros engine test

---
+++ tests/view/JmarrerosEngineTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Jmarreros\Test\view;
+
+use Jmarreros\View\JmarrerosEngine;
+use PHPUnit\Framework\TestCase;
+
+class JmarrerosEngineTest extends TestCase {
+	public function testRenderTemplateWithParams() {
+		$parameter1 = "Test 1";
+		$parameter2 = 2;
+
+		$expected = "<html>
+	<body>
+		<h1>$parameter1</h1>
+		<h1>$parameter2</h1>
+	</body>
+</html>";
+
+		$engine  = new JmarrerosEngine( __DIR__ . "/views" );
+		$content = $engine->render( "test", compact( 'parameter1', 'parameter2' ), 'layout' );
+
+		$this->assertEquals(
+			preg_replace("/\s*/", "", $expected),
+			preg_replace("/\s*/", "", $content));
+	}
+}
