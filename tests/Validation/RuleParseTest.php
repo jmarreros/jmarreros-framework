b0c1d3ff05499d1ce89e4b122ffb5398931ebf7d
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2023-01-31 11:53:22 -0500

    Test Validator rules

---
+++ tests/Validation/RuleParseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jmarreros\Test\Validation;
+
+use Jmarreros\Validation\Rule;
+use Jmarreros\Validation\Rules\Email;
+use Jmarreros\Validation\Rules\Number;
+use Jmarreros\Validation\Rules\Required;
+use PHPUnit\Framework\TestCase;
+
+class RuleParseTest extends  TestCase {
+	protected function setUp(): void {
+		Rule::loadDefaultRules();
+	}
+
+	public function basicRules() {
+		return [
+			[Email::class, "email"],
+			[Required::class, "required"],
+			[Number::class, "number"],
+		];
+	}
+
+	/**
+	 * @dataProvider basicRules
+	 */
+	public function test_parse_basic_rules($class, $name) {
+		$this->assertInstanceOf($class, Rule::from($name));
+	}
+}
--- tests/Validation/ValidatorTest.php
+++ tests/Validation/ValidatorTest.php
@@ -3,6 +3,7 @@
 namespace Jmarreros\Test\Validation;
 
 use Jmarreros\Validation\Exceptions\ValidationException;
+use Jmarreros\Validation\Rule;
 use Jmarreros\Validation\Rules\Email;
 use Jmarreros\Validation\Rules\LessThan;
 use Jmarreros\Validation\Rules\Number;
@@ -12,53 +13,83 @@ use Jmarreros\Validation\Validator;
 use PHPUnit\Framework\TestCase;
 
 class ValidatorTest extends TestCase {
+
+	// Se ejecuta antes de cada test
+	protected function setUp(): void {
+		Rule::LoadDefaultRules();
+	}
+
 	public function test_basic_validation_passes() {
 		$data = [
 			"email" => "test@test.com",
 			"other" => 2,
-			"num" => 3,
-			"foo" => 5,
-			"bar" => 4
+			"num"   => 3,
+			"foo"   => 5,
+			"bar"   => 4
 		];
 
 		$rules = [
 			"email" => new Email(),
 			"other" => new Required(),
-			"num" => new Number(),
+			"num"   => new Number(),
 		];
 
 		$expected = [
 			"email" => "test@test.com",
 			"other" => 2,
-			"num" => 3,
+			"num"   => 3,
 		];
 
-		$v = new Validator($data);
+		$v = new Validator( $data );
 
-		$this->assertEquals($expected, $v->validate($rules));
+		$this->assertEquals( $expected, $v->validate( $rules ) );
 	}
 
 	public function test_throws_validation_exception_on_invalid_data() {
-		$this->expectException(ValidationException::class);
-		$v = new Validator(["test" => "test"]);
-		$v->validate(["test" => new Number()]);
+		$this->expectException( ValidationException::class );
+		$v = new Validator( [ "test" => "test" ] );
+		$v->validate( [ "test" => new Number() ] );
 	}
 
 	/**
 	 * @depends test_basic_validation_passes
 	 */
 	public function test_multiple_rules_validation() {
-		$data = ["age" => 20, "num" => 3, "foo" => 5];
+		$data = [ "age" => 20, "num" => 3, "foo" => 5 ];
 
 		$rules = [
-			"age" => new LessThan(100),
-			"num" => [new RequiredWith("age"), new Number()],
+			"age" => new LessThan( 100 ),
+			"num" => [ new RequiredWith( "age" ), new Number() ],
 		];
 
-		$expected = ["age" => 20, "num" => 3];
+		$expected = [ "age" => 20, "num" => 3 ];
 
-		$v = new Validator($data);
+		$v = new Validator( $data );
 
-		$this->assertEquals($expected, $v->validate($rules));
+		$this->assertEquals( $expected, $v->validate( $rules ) );
+	}
+
+	public function test_overrides_error_message_correctly() {
+		$data  = [ "email" => "test@", "num1" => "not a number" , "num2" => "ss"];
+		$rules = [
+			"email" => "email",
+			"num1"  => "number",
+			"num2"  => [ "required", "number" ],
+		];
+
+		$messages = [
+			"email" => [ "email" => "Email test message" ],
+			"num1"  => [ "number" => "Test number message" ],
+			"num2"  => [ "number" => "Test number message again" ]
+		];
+
+		$v = new Validator( $data );
+
+		try {
+			$v->validate( $rules, $messages );
+			$this->fail( "Did not throw validation exception" );
+		} catch ( ValidationException $e ) {
+			$this->assertEquals( $messages, $e->errors() );
+		}
 	}
 }
